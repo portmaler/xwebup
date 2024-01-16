08f7aa473700f112760924a3e80380ea48a52bf2
portmaler <56043999+portmaler@users.noreply.github.com>
2024-01-16 14:47:16 +0100

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import DarkTheme from "../layouts/Dark";
+import Demos from "./demos";
+import Homepage3 from "./home/home3-dark";
+
+export default function Home() {
+  return (
+    <DarkTheme>
+      <Homepage3 />
+    </DarkTheme>
+  );
+}
